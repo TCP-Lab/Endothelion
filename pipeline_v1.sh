a96de7b369634a5a24f05f469474648a38175989
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-03-01 16:43:55 +0100

    Generalized shell pipeline to find and cycle over multiple targets

--- pipeline_v1.sh
+++ pipeline_v1.sh
@@ -1,8 +1,42 @@
+#!/bin/bash
 
+# ============================================================================ #
+#  The Endothelion Pipeline
+# ============================================================================ #
 
+# --- General settings and variables -------------------------------------------
 
-Rscript endo_profiler.R \
-	"./Lines/hCMEC D3/GSE76528_TPM.tsv" \
-	"TPM" \
-	true \
-	"./ICT_set.csv"
+set -e # "exit-on-error" shell option
+set -u # "no-unset" shell option
+
+# --- Pipeline starts here------------------------------------------------------
+
+in_path="./data/in"
+count_type="TPM"
+threshold=true
+GOIs="./ICT_set.csv"
+
+# Looping through files with spaces in their names or paths is not such a
+# trivial thing...
+OIFS="$IFS"
+IFS=$'\n'
+for count_matrix in `find "${in_path}" -maxdepth 4 \
+	-type f -iname "*.tsv" | sort`
+do
+	echo "Analyzing file: ${count_matrix}"
+	Rscript endo_profiler.R \
+		"$count_matrix" \
+		"$count_type" \
+		"$threshold" \
+		"$GOIs"
+	echo
+done
+IFS="$OIFS"
+echo "DONE"
+exit 0 # Success exit status
+
+# GSE76528_TPM
+# GSE138309_TPM
+# GSE139133_TPM
+# GSE195781_TPM
+# GSE205739_TPM
