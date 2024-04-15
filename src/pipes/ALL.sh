35d58d10a3395438e7c0f9e60c2ad3c229a6dd6d
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-04-15 18:14:18 +0200

    Refactored pipelines (moved some docs to ./README.md)

--- src/pipes/ALL.sh
+++ src/pipes/ALL.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
 # ==============================================================================
-#? Global ICT absolute expression
+#? Global ICT absolute expression profiling
 #?
-#? This pipeline is used to visualize the absolute expression of some genes of
-#? interest (GOIs) out of a count matrix containing the whole genome and many
-#? biological replicates. The `endo_profiler` R script is used for this task.
-#? It sequentially takes as its first argument all the TSV files found in the
-#? input folder (including possible subfolders). Most of the parameter values
-#? are retrieved from the `runtime_options` JSON file. Namely,
-#?  1. *in_path*: the input folder (`./data/in` in a *Kerblam!* project);
-#?  2. *count_type*: count units (usually *TPMs* for absolute expression);
-#?  3. *threshold_adapt*: should a GMM be used to find the expression threshold?
-#?  4. *threshold_value*: threshold value or the number of Gaussian components;
-#?  5. *GOIs*: path to the list of GOIs.
-#? Specifically, when `threshold_adapt == true`, the integer entered as the
-#? `threshold_value` indicates the number of Gaussian components to be used in
-#? the mixture for the adaptive calculation of the expression threshold. In
-#? contrast, when `threshold_adapt == false`, the `threshold_value` is the real
-#? value to be used as constant threshold in all the experiments.
-#? The last parameter of `endo_profiler.R` (i.e., the output directory) is not
-#? retrieved from the JSON option file. On the contrary it is hard-coded so as
-#? to recreate within the `./data/out` directory (*Kerblam!* standard), a
-#? filesystem similar to that found in `./data/in`, but containing only the
-#? results of the analysis.
-#? More details on the actual analysis algorithm and its expected arguments can
+#? This pipeline performs the *Phase I* Endothelion task for ICT absolute
+#? expression assessment applied to all the available datasets.
+#? The *endo_profiler* Bash wrapper is invoked to parses the `runtime_options`
+#? JSON file and get the user-defined parameter values set therein (see
+#? `./README.md` for details). Then, it searches the input directory (including
+#? possible subfolders) for all the available TSV count matrices, which will be
+#? sequentially fed to the related R script for actual data analysis. Note that
+#? the last parameter of `endo_profiler.R` (i.e., the output directory) is not
+#? retrieved from the JSON option file. On the contrary it is *hard-coded* by
+#? the wrapper so as to recreate within the `./data/out` directory (*Kerblam*!
+#? standard), a filesystem closely tracing the one found in `./data/in`, but
+#? containing only the results of the analysis.
+#? Further details about the analysis algorithm and its expected arguments can
 #? be found in the header of the R script file `./src/endo_profiler.R`.
 # ==============================================================================
 
-# --- General settings and variables -------------------------------------------
+# --- The pipeline starts here -------------------------------------------------
+echo -e "\n\e[1;32mSTARTING GLOBAL PROFILING\e[0m\n"
 
+# Global ICT absolute expression profiling
 bash ./src/endo_profiler_wrap.sh "./data/in/"
+
+echo -e "\e[1;32mPIPELINE SUCCESSFULLY COMPLETED\e[0m\n"
