4bd0fda6297a2c26cf5f492cb6b6ad46b5717d5e
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-04-16 18:05:54 +0200

    Added some comments to 'make_test_dataset.R'

--- src/utils/make_test_dataset.R
+++ src/utils/make_test_dataset.R
@@ -3,25 +3,31 @@
 # For reproducibility
 set.seed(7)
 
+# Parse arguments
 args = commandArgs(trailingOnly = TRUE)
-
 target_path <- args[1]
 gois_path <- args[2]
 proj <- args[3]
 
-read.delim(file.path(target_path, paste0(proj, "_CountMatrix_genes_TPM.tsv"))) -> df
+# Load files
+read.delim(file.path(target_path,
+                     paste0(proj, "_CountMatrix_genes_TPM.tsv"))) -> df
 read.csv(gois_path, header = FALSE) |> unlist() -> GOIs
 
+# Find GOIs within the count matrix
 logic <- df$SYMBOL %in% GOIs
-logic[sample(1:length(logic),5e3)] <- TRUE
+# Add 5000 more random genes
+# (statistically, some them will be GOIs already included in the previous step)
+logic[sample(1:length(logic), 5e3)] <- TRUE
+# Subset the count matrix to get a slim version of it
 df <- df[logic,]
 
 file_name <- paste0("test_", proj, "_CountMatrix_genes_TPM.tsv")
-
 cat("----------------------------------------------------\n")
 cat("Saving: ", file_name,
     "\n         Test count matrix with", dim(df)[1]," rows.\n")
 cat("----------------------------------------------------\n\n")
 
+# Save the slim matrix within the target path
 write.table(df, file.path(target_path, file_name),
             quote = FALSE, sep = "\t", row.names = FALSE)
