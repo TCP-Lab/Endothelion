50f46e6aa027e928a9ee84f7e61617454f4f48d3
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-04-30 18:19:40 +0200

    `seq_loader.R`> Code refacoring and minor changes (now runs are sorted by their `ena_run` IDs in the list object).

--- src/seq_loader.R
+++ src/seq_loader.R
@@ -1,3 +1,32 @@
+
+# Assumptions:
+#
+# 1 - Both data and metadata have file names starting with the same series
+#     (experiment, project) ID (both GEO and ENA are fine), followed by an
+#     underscore ("_") and either the distinctive string "CountMatrix" or "meta"
+#     if they are counts or metadata, respectively. Additional characters are
+#     allowed after this pattern, including either CSV or TSV file extensions.
+#     Examples of good file name patterns are:
+#
+#         GSE205739_CountMatrix_genes_TPM.tsv
+#         GSE76528_meta.csv
+#
+# 2 - Metadata files have proper column names as table header, among which
+#     `ena_run` is mandatory, as the only ID that is certainly unique to each
+#     file.
+#
+# 3 - Count tables have proper column names as header, among which a mandatory
+#     ID column mtching the regex "gene.*id" or "transcript.*id" (usually being
+#     "gene_id" or "transcript_id", respectively).
+#
+# 4 - Columns in cout table are named according to the `ena_run` ID
+
+
+
+
+library(dplyr)
+
+
 setwd("../data/in/Lines/hCMEC_D3/")
 
 
@@ -7,16 +36,11 @@ file_list <- list.files(path = target_dir,
                         pattern = "\\.[ct]sv$",
                         full.names = FALSE)
 
-# Both data and metadata file names have to start with the same series
-# (experiment) ID, followed by an underscore and either "CountMatrix" or "meta"
-# if counts or metadata, respectively. More characters are allowed after them,
-# including ANY file extension.
-# meta need to have proper column names as heading, with ena_run as mandatory
-#
+
 file_list |> sub("_.*$", "", x=_) |> unique() -> series_IDs
 
 
-
+# Automatically adapt to CSV or TSV format
 read.xsv <- function(file, header = TRUE) {
   if (grepl(".csv$", file, ignore.case = TRUE)) {
     read.csv(file, header = header)
@@ -36,26 +60,27 @@ read.xsv <- function(file, header = TRUE) {
 
 lapply(series_IDs, function(series_ID, files = file_list) {
   
-  files |> grep(paste0(series_ID, "_CountMatrix.*"), x=_, value = TRUE) |>
-    read.xsv() -> counts_df
+  # Load count data and associated metadata
+  series_ID %|+>% "_CountMatrix.*" |> grep(files, value=T) |> read.xsv() -> counts_df
+  series_ID %|+>% "_meta.*" |> grep(files, value=T) |> read.xsv() -> meta_df
   
-  files |> grep(paste0(series_ID, "_meta.*"), x=_, value = TRUE) |>
-    read.xsv() -> meta_df
-  
-  # Convert rows to list
+  # Sort by `ena_run` and convert rows to list
+  meta_df |> arrange(ena_run) -> meta_df
   meta_df |> split(seq(nrow(meta_df))) |> setNames(meta_df$ena_run) -> meta_list
   
+  # Find the ID column in count table
   grep("gene.*id|transcript.*id", colnames(counts_df)) -> ids_index
   
-  lapply(meta_list, function(run){
-    
+  # Add read counts and gene IDs to each run in `meta_list` to build up the
+  # `series` object
+  lapply(meta_list, function(run, counts_df = counts_df, ids_index = ids_index){
+    # Find the correct SRR run column in count table
     run$ena_run |> grep(colnames(counts_df)) -> counts_index
-    
-    run |> append(list(
-      gene = data.frame(IDs = counts_df[ids_index],
-                        counts = counts_df[counts_index])))
-    
+    # Add gene IDs and read count data (if present) as a data frame to each run 
+    run |> append(list(gene = data.frame(IDs = counts_df[ids_index],
+                                         counts = counts_df[counts_index])))
     }) -> series
+  
   
   meta_df$ena_run |> paste(collapse = "|") |>
     grep(colnames(counts_df), invert = TRUE) -> annot_index
