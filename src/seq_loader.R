7f19b4d6cd80cf5205ef83f9e819632f46f0d91f
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-05-03 19:01:26 +0200

    `seq_loader.R`> Uncoupled 'bioSeries' and 'bioModel' contructors.

--- src/seq_loader.R
+++ src/seq_loader.R
@@ -5,6 +5,8 @@
 # 
 # Assumptions:
 #
+# 0 - Each model lives in a separate folder containing all series files for it.
+#
 # 1 - Both data and metadata have file names starting with the same series
 #     (aka experiment or project) ID (both GEO and ENA are fine), followed by an
 #     underscore ("_") and either the distinctive string "CountMatrix" or "meta"
@@ -31,6 +33,7 @@
 
 library(dplyr) # arrange, select
 library(rlang)  # Injection operator `!!`
+library(magrittr) #  for pipe assignment operator %<>% and Aliases
 
 # Automatically adapt to CSV or TSV format
 read.xsv <- function(file, header = TRUE) {
@@ -49,54 +52,99 @@ read.xsv <- function(file, header = TRUE) {
 
 
 
+check_filenames <- function(series_ID, files, pattern) {
+  
+  skip_this <- FALSE
+  
+  # Find file pair
+  series_ID %+% "_" |> grep(files, value=T) -> run_pair
+  
+  if (length(run_pair) != 2) {
+    "Wrong number of files in series " %+% series_ID %+% "... skip it!" |> warning()
+    skip_this <- TRUE
+    
+  } else {
+    # Check if one element matches 'count_pattern' AND the other matches
+    # 'meta_pattern', OR vice-versa. NOTE: the logic below may not seem
+    # immediately self-evident, but it's fast and, trust me (trust you), it
+    # works: 'sapply' returns an identity matrix iff condition is met. Try it.
+    sapply(pattern,\(rgx) grepl(rgx, run_pair, ignore.case=T)) |> equals(diag(2)) |> all() -> matching
+    if (not(matching)) {
+      "Bad filename pair in series " %+% series_ID %+% "... skip it!" |> warning()
+      skip_this <- TRUE
+    }
+  }
+  return(skip_this)
+}
+  
+  
+  
+  
+
+# To make code lighter, Anonymous  functions used in *apply or pipes are NOT self-contained
+# instead they may access variables from the outer scope
+
+
+new_bioSeries <- function(series_ID, target_dir = ".") {
+
+  # Get all file names inside target directory
+  list.files(path = target_dir, pattern = "\\.[ct]sv$") |> sort() -> files
+  # Patterns to match
+  pattern <- c(count = "_countmatrix.*", meta = "_meta.*")
+  
+  # Load data-metadata pair (also sort metadata by `ena_run`)
+  file.path(target_dir,
+            series_ID %+% pattern["count"] |> grep(files, ignore.case=T, value=T)
+            ) |> read.xsv() -> counts_df
+  file.path(target_dir,
+            series_ID %+% pattern["meta"]  |> grep(files, ignore.case=T, value=T)
+            ) |> read.xsv() |> arrange(ena_run) -> meta_df
+  
+  # Convert rows to list
+  meta_df |> split(seq(nrow(meta_df))) |> setNames(meta_df$ena_run) -> meta_list
+  
+  # Find ID column in `counts_df`
+  "gene.*id|transcript.*id" |> grep(colnames(counts_df), ignore.case=T) -> ids_index
+  
+  # Build up a `series` object
+  lapply(meta_list, function(run) {
+    # Look for run's count data...
+    run$ena_run |> grep(colnames(counts_df)) -> count_index
+    # ...and add both counts (if present) and IDs to each run-list as a data frame
+    counts_df |> select(IDs = !!ids_index, counts = !!count_index) |>
+      list(genes=_) |> append(run, values=_)
+    }) -> series
+
+  # Add annotation to series
+  meta_df$ena_run |> paste(collapse = "|") |> grep(colnames(counts_df), invert=T) -> annot_index
+  counts_df |> select(!!annot_index) |> list(annotation=_) |> append(series, values=_) -> series
+  
+  # Make the `series` list an S3 object and return it
+  structure(series, class = "bioSeries")
+}
 
 
 
+new_bioModel <- function(target_dir = ".") {
 
-
-
-new_bioModel <- function(target_dir) {
-
-  # Get all files inside target directory
-  file_list <- list.files(path = target_dir,
-                          pattern = "\\.[ct]sv$",
-                          full.names = FALSE)
-
-
-
-
+  # Get all file names inside target directory
+  list.files(path = target_dir, pattern = "\\.[ct]sv$") |> sort() -> files
   # Clean series IDs
-  file_list |> sub("_.*$", "", x=_) |> sort() |> unique() -> series_IDs
+  files |> sub("_.*$", "", x=_) |> sort() |> unique() -> series_IDs
+  if (length(series_IDs) == 0) {
+    "Cannot find series files in " %+% target_dir %+% ". Stop constructor." |> stop()
+  }
+  
+  # Patterns to match
+  pattern <- c(count = "_countmatrix.*", meta = "_meta.*")
+  
+  # Check filenames for series not to include
+  to_skip <- vector(mode = "logical", length = 0)
+  sapply(series_IDs, check_filenames, files, pattern) -> to_skip
+  series_IDs <- series_IDs[not(to_skip)]
   
   # Build up the bioModel object
-  lapply(series_IDs, function(series_ID, files = file_list) {
-    
-    # Load data-metadata pairs (also sort metadata by `ena_run`)
-    series_ID %+% "_CountMatrix.*" |> grep(files, ignore.case=T, value=T) |> read.xsv() -> counts_df
-    series_ID %+% "_meta.*" |> grep(files, ignore.case=T, value=T) |> read.xsv() |> arrange(ena_run) -> meta_df
-    
-    # Convert rows to list
-    meta_df |> split(seq(nrow(meta_df))) |> setNames(meta_df$ena_run) -> meta_list
-    
-    # Find ID column in `counts_df`
-    "gene.*id|transcript.*id" |> grep(colnames(counts_df), ignore.case=T) -> ids_index
-    
-    # Build up a `series` object
-    lapply(meta_list, function(run, coun_df = counts_df, id_ndx = ids_index) {
-      # Look for run's count data...
-      run$ena_run |> grep(colnames(coun_df)) -> coun_ndx
-      # ...and add both counts (if present) and IDs to each run-list as a data frame
-      coun_df |> select(IDs = !!id_ndx, counts = !!coun_ndx) |> list(genes=_) |> append(run, values=_)
-    }) -> series
-    
-    # Add annotation to each series
-    meta_df$ena_run |> paste(collapse = "|") |> grep(colnames(counts_df), invert=T) -> annot_index
-    counts_df |> select(!!annot_index) |> list(annotation=_) |> append(series, values=_) -> series
-    
-    # Make the series list an S3 object and return it
-    structure(series, class = "bioSeries")
-    
-  }) |> setNames(series_IDs) -> base_list
+  lapply(series_IDs, new_bioSeries, target_dir) |> setNames(series_IDs) -> base_list
   
   # Make the base list an S3 object and return it
   structure(base_list, class = "bioModel")
