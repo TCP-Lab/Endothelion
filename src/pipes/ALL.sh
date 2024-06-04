519c05b68179e739bdac2bed7f07034bc6865585
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-06-04 10:04:16 +0200

    Updated pipes' docs.

--- src/pipes/ALL.sh
+++ src/pipes/ALL.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 # ==============================================================================
-#? ICT absolute expression analysis in all endo-models
+#? ICT+ absolute expression analysis in all endo-models
 #?
-#? This pipeline performs the *Phase I* Endothelion task for ICT absolute
+#? This pipeline performs the *Phase I* Endothelion task for ICT+ absolute
 #? expression assessment applied to all the available datasets.
-#? The *endo_profiler* Bash wrapper is invoked to parses the `runtime_options`
+#? The *endo_profiler* Bash wrapper is invoked to parse the `runtime_options`
 #? JSON file and get the user-defined parameter values set therein (see
 #? `./README.md` for details). Then, it searches the input directory (including
 #? possible subfolders) for all the available TSV count matrices, which will be
-#? sequentially fed to the related R script for actual data analysis. Note that
-#? the last parameter of `endo_profiler.R` (i.e., the output directory) is not
-#? retrieved from the JSON option file. On the contrary it is *hard-coded* by
-#? the wrapper so as to recreate within the `./data/out` directory (*Kerblam*!
-#? standard), a filesystem closely tracing the one found in `./data/in`, but
-#? containing only the results of the analysis.
+#? sequentially fed to the associated R script for actual data analysis.
 #? Further details about the analysis algorithm and its expected arguments can
 #? be found in the header of the R script file `./src/endo_profiler.R`.
 # ==============================================================================
